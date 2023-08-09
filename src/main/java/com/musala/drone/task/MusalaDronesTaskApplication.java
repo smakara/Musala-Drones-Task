9bfba7e2c786300d8999e5186ce9cfb91b98d4c2
smakara <shepherdmakara@gmail.com>
2023-08-09 03:47:25 +0200

    project setup

---
+++ src/main/java/com/musala/drone/task/MusalaDronesTaskApplication.java
@@ -0,0 +1,13 @@
+package com.musala.drone.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalaDronesTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusalaDronesTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/drone/task/MusalaDronesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.drone.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusalaDronesTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
